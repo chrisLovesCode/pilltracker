c6034150b542acec68042746d5d52dee7778d106
Chris Vaupel <chrisvaupel@gmail.com>
2026-02-10 14:15:31 +0100

    fix: explicitly register sqlite plugin

--- android/app/src/main/java/com/pilltracker/app/MainActivity.java
+++ android/app/src/main/java/com/pilltracker/app/MainActivity.java
@@ -2,11 +2,14 @@ package com.pilltracker.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.database.sqlite.CapacitorSQLitePlugin;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    // Explicit registration makes plugin availability more robust across devices/build variants.
+    registerPlugin(CapacitorSQLitePlugin.class);
     registerPlugin(PrintPlugin.class);
   }
 }
